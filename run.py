b3fd7a4b7f6ece3284a95934a0aa53dae7dea75b
Shemar Jones <jones.shemar.sj@gmail.com>
2021-06-15 21:42:58 -0500

    Testing Flask SSL Certificates

--- run.py
+++ run.py
@@ -3,4 +3,7 @@
 from app import app
 
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", port=5000)
+    #certificate and key files
+    # context = ('local.crt', 'local.key')
+
+    app.run(debug=True, host="0.0.0.0", port=5000, ssl_context='adhoc')
